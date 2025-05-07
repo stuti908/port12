b4196a01e62cdddc818050538da43c59a0180afa
Ekta sajwan <148518959+stuti908@users.noreply.github.com>
2025-05-07 17:39:52 +0530

    xyz

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './About.css';
 
+
 const About = () => {
   return (
-    <section className="about">
-      <h2>About Me</h2>
-      <p>
- Hi, I’m a Software Developer with 8 months of industry experience in Embedded Systems, specializing in UART-based serial communication (RS485/RS232) and C programming for hardware modules.
-Alongside, I’ve also worked on Full Stack Web Development, building responsive and interactive web applications using React.js, Node.js, and MongoDB, with a strong eye for design through UI/UX tools like Figma .I’m passionate about combining low-level systems programming with modern web technologies to build smart, scalable, and user-friendly tech solutions.</p>
-    </section>
+    <div className="about-container">
+      <div className="overlay-panel">
+        <h2>About Me</h2>
+        <p>
+          Hi, I’m a Software Developer with 8 months of industry experience in Embedded Systems,
+          specializing in UART-based serial communication (RS485/RS232) and C programming for
+          hardware modules.
+        </p>
+        <p>
+          Alongside, I’ve also worked on Full Stack Web Development, building responsive and
+          interactive web applications using React.js, Node.js, and MongoDB, with a strong eye for
+          design through UI/UX tools like Figma. I’m passionate about combining low-level systems
+          programming with modern web technologies to build smart, scalable, and user-friendly tech
+          solutions.
+        </p>
+      </div>
+    </div>
   );
 };
 
